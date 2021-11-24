63694305df93669d0a216f197eed8e33ed221be1
Igor Chevdar <igor.chevdar@jetbrains.com>
2021-11-24 15:58:39 +0500

    Review fix

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/GeneralNativeIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/GeneralNativeIT.kt
@@ -724,21 +724,12 @@ class GeneralNativeIT : BaseGradleIT() {
             fun assertTestResultsAnyOf(
                 @TestDataFile assertionFile1Name: String,
                 @TestDataFile assertionFile2Name: String,
-                @TestDataFile assertionFile3Name: String?,
                 vararg testReportNames: String
             ) {
                 try {
                     assertTestResults(assertionFile1Name, *testReportNames)
                 } catch (e: AssertionError) {
-                    if (assertionFile3Name == null)
-                        assertTestResults(assertionFile2Name, *testReportNames)
-                    else {
-                        try {
-                            assertTestResults(assertionFile2Name, *testReportNames)
-                        } catch (e: AssertionError) {
-                            assertTestResults(assertionFile3Name, *testReportNames)
-                        }
-                    }
+                    assertTestResults(assertionFile2Name, *testReportNames)
                 }
             }
 
@@ -746,27 +737,30 @@ class GeneralNativeIT : BaseGradleIT() {
             // If, in the test project, preset name was updated,
             // update accordingly test result output for Gradle6.6+
             val testGradleVersion = project.chooseWrapperVersionOrFinishTest()
-            val expectedTestResults = if (GradleVersion.version(testGradleVersion) < GradleVersion.version("6.6")) {
-                listOf(
+            val expectedHostTestResult: String
+            val expectedIOSTestResults: List<String>
+            if (GradleVersion.version(testGradleVersion) < GradleVersion.version("6.6")) {
+                expectedHostTestResult = "testProject/native-tests/TEST-TestKt_pre6.6.xml"
+                expectedIOSTestResults = listOf(
                     "testProject/native-tests/TEST-TestKt_pre6.6.xml",
                     "testProject/native-tests/TEST-TestKt-iOSsim_pre6.6.xml",
                 )
             } else {
-                listOf(
-                    "testProject/native-tests/TEST-TestKt.xml",
+                expectedHostTestResult = "testProject/native-tests/TEST-TestKt.xml"
+                expectedIOSTestResults = listOf(
                     "testProject/native-tests/TEST-TestKt-iOSsim.xml",
                     "testProject/native-tests/TEST-TestKt-iOSsim_wWarn.xml",
                 )
             }
-            assertTestResults(expectedTestResults.first(), hostTestTask)
+
+            assertTestResults(expectedHostTestResult, hostTestTask)
             // K/N doesn't report line numbers correctly on Linux (see KT-35408).
             // TODO: Uncomment when this is fixed.
             //assertStacktrace(hostTestTask, "host")
             if (HostManager.hostIsMac) {
                 assertTestResultsAnyOf(
-                    expectedTestResults[0],
-                    expectedTestResults[1],
-                    if (expectedTestResults.size == 2) null else expectedTestResults[2],
+                    expectedIOSTestResults[0],
+                    expectedIOSTestResults[1],
                     "iosTest"
                 )
                 assertStacktrace("iosTest", "ios")
